64469ecce4f4af626033bbd6b0fb513fb73e5c28
Léo <leo126com@gmail.com>
2024-12-07 22:24:02 +0100

    commit modifié

--- streamlit3.py
+++ streamlit3.py
@@ -2,7 +2,10 @@ import streamlit as st
 from streamlit_authenticator import Authenticate
 
 # Nos données utilisateurs doivent respecter ce format
-
+st.set_page_config(
+    page_title = "Multipage App",
+    page_icon = " :wave:",
+)
 lesDonneesDesComptes = {'usernames': {'utilisateur': {'name': 'utilisateur',
    'password': 'utilisateurMDP',
    'email': 'utilisateur@gmail.com',
@@ -31,23 +34,55 @@ if st.session_state["authentication_status"]:
   accueil()
   # Le bouton de déconnexion
   authenticator.logout("Déconnexion")
-
 elif st.session_state["authentication_status"] is False:
     st.error("L'username ou le password est/sont incorrect")
 elif st.session_state["authentication_status"] is None:
     st.warning('Les champs username et mot de passe doivent être remplie')
 
+
 import streamlit as st
 # Importation du module
 from streamlit_option_menu import option_menu
+
 # Création du menu qui va afficher les choix qui se trouvent dans la variable options
 selection = option_menu(
             menu_title=None,
             options = ["Accueil", "Photos"]
         )
-# On indique au programme quoi faire en fonction du choix
+
 if selection == "Accueil":
     st.write("Bienvenue sur la page d'accueil !")
 elif selection == "Photos":
     st.write("Bienvenue sur mon album photo")
-# ... et ainsi de suite pour les autres pages
+
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  st.header("A cat")
+  st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+  st.header("A dog")
+  st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+  st.header("An owl")
+  st.image("https://static.streamlit.io/examples/owl.jpg")
+
+import streamlit as st
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    icon= st.button("Click here 🦊")
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
